0420062521e1e67b2ea652f82d0a5933889efc54
Dave Page <dpage@pgadmin.org>
2021-07-15 11:16:34 +0100

    Cleanup the Starlight provider ready for new code.

--- providers/starlight.py
+++ providers/starlight.py
@@ -21,26 +21,6 @@ class StarlightProvider(AbsProvider):
         parsers = self.parser.add_subparsers(help='Starlight command help',
                                              dest='command')
 
-        # Create the Deploy command parser
-        parser_deploy = parsers.add_parser('deploy-cluster',
-                                           help='deploy a new cluster')
-        parser_deploy.add_argument('--name', required=True,
-                                   help='name of the cluster')
-        parser_deploy.add_argument('--type', required=True,
-                                   help='machine type for the cluster nodes')
-
-        # Create the Types command parser
-        parser_types = parsers.add_parser('list-types',
-                                          help='list available instance types')
-
-    def cmd_deploy_cluster(self, args):
-        print(
-            'Deploying Starlight Cluster "{}" using instance type {}...'.format(
-                args.name, args.type))
-
-    def cmd_list_types(self, args):
-        print('Listing Starlight Types...')
-
 
 def load():
     """ Loads the current provider """
